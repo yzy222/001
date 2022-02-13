d1ea6b6fb4da11fd53c5efd9407582a97c8be7b7
yzy25138 <1463096380@qq.com>
2022-02-13 20:43:26 +0800

    eee7

--- 001/src/com/io/FileoutputstreamText.java
+++ 001/src/com/io/FileoutputstreamText.java
@@ -3,17 +3,22 @@ package com.io;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileoutputstreamText {
     public static void main(String[] args) {
+//        void write(byte[] b) 写入
+//        void write(byte[] b, int off, int len) 写入
+        //FileOutputStream(File file, boolean append) 追加
         FileOutputStream fos=null;
         try {
             fos = new FileOutputStream("myfile");
             byte[] bytes={97,98,99,100};
             fos.write(bytes);
+            String s="中国人";
+            byte[] bytes1=s.getBytes();
+            fos.write(bytes1);
             fos.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
